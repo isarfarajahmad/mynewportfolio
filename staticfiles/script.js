1f67263ef2b595ca915cfd3ec79b3da72d59823d
Md Sarfaraj Ahmad <isarfarajahmad@gmail.com>
2020-09-16 17:50:21 +0530

    first commit

---
+++ staticfiles/script.js
@@ -0,0 +1,43 @@
+console.log("Its working");
+
+let theme = localStorage.getItem("theme");
+
+if (theme == null) {
+  setTheme("light");
+} else {
+  setTheme(theme);
+}
+
+let themeDots = document.getElementsByClassName("theme-dot");
+
+for (var i = 0; themeDots.length > i; i++) {
+  themeDots[i].addEventListener("click", function () {
+    let mode = this.dataset.mode;
+    console.log("Option clicked:", mode);
+    setTheme(mode);
+  });
+}
+
+function setTheme(mode) {
+  if (mode == "light") {
+    document.getElementById("theme-style").href = "static/style.css";
+  }
+
+  if (mode == "pink") {
+    document.getElementById("theme-style").href = "static/pink.css";
+  }
+
+  if (mode == "green") {
+    document.getElementById("theme-style").href = "static/green.css";
+  }
+
+  if (mode == "orange") {
+    document.getElementById("theme-style").href = "static/orange.css";
+  }
+
+  if (mode == "dark") {
+    document.getElementById("theme-style").href = "static/dark.css";
+  }
+
+  localStorage.setItem("theme", mode);
+}
